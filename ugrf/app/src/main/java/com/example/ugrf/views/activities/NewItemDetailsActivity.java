b8023f37445b82e2bdda927cbb752d5cb0a800a2
danialaa <d.a.mahdaly@outlook.com>
2020-08-06 06:14:13 +0200

    item registration

--- ugrf/app/src/main/java/com/example/ugrf/views/activities/NewItemDetailsActivity.java
+++ ugrf/app/src/main/java/com/example/ugrf/views/activities/NewItemDetailsActivity.java
@@ -1,23 +1,29 @@
 package com.example.ugrf.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.ugrf.R;
 
 public class NewItemDetailsActivity extends AppCompatActivity {
+    private AppCompatSpinner categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item_details);
+
+        categorySpinner = findViewById(R.id.categorySpinner);
     }
 
     public void backToHome(View view) {
     }
 
     public void publish(View view) {
+        startActivity(new Intent(NewItemDetailsActivity.this, SellerDashboardActivity.class));
     }
 }
